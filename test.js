b0b888ac97cc1d1d41c88205cae8476c2891bd75
rajmayank <mayank9856@gmail.com>
2019-05-03 10:10:55 +0530

    Added Test Cases

---
+++ test.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('2 + 2 should be 4', function() {
+      assert.equal( (2 + 2), 4);
+    });
+  });
+});
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('1 + 5 should be 6', function() {
+      assert.equal( (1 + 5), 6);
+    });
+  });
+});
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('\'5\' == 5 should be true', function() {
+      assert.equal( ('5' == 5), true);
+    });
+  });
+});
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('\'5\' === 5 should be false', function() {
+      assert.equal( ('5' === 5), false);
+    });
+  });
+});
